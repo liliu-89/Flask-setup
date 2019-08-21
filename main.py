39450ab855766fe05f543db7a6e6efd9f760b1f1
smartninja-li <52677222+smartninja-li@users.noreply.github.com>
2019-08-21 21:02:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/user")
+def impressum():
+    return render_template("user.html")
+
+app.run()
